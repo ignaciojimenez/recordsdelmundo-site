a9dd749d6880e551fac40fdd4eb320ea50c4358b
Unknown <i.jimenezpi@gmail.com>
2018-03-19 21:05:43 +0100

    playMp3 changes

--- estoescasa/js/playMp3.js
+++ estoescasa/js/playMp3.js
@@ -1,49 +1,49 @@
 
 // Change these two lines to modify your PLAY and STOP buttons.
-var mp3PlayButton = "http://www.estoescasa.com/images/play2.gif";
-var mp3StopButton = "http://www.estoescasa.com/images/stop2.gif";
+var mp3PlayButton = "../images/play2.gif";
+var mp3StopButton = "../images/stop2.gif";
 
 
 
-if(typeof(TagTooga) == 'undefined') 
-	TagTooga = {} 
+if(typeof(TagTooga) == 'undefined')
+	TagTooga = {}
 
-TagTooga.Mp3 = 
-{ 
-	playimg: null, 
-	player: null, 
+TagTooga.Mp3 =
+{
+	playimg: null,
+	player: null,
 
-	go: function() 
-		{ 
+	go: function()
+		{
 		var all = document.getElementsByTagName('a');
-		for (var i = 0, o; o = all[i]; i++) 
-			{ 
-			if(o.href.match(/\.mp3$/i)) 
-				{ 
+		for (var i = 0, o; o = all[i]; i++)
+			{
+			if(o.href.match(/\.mp3$/i))
+				{
 				var img = document.createElement('img');
 				img.src = mp3PlayButton;
-				img.title = 'listen'; 
-				//img.height = img.width = 12; 
+				img.title = 'listen';
+				//img.height = img.width = 12;
 				img.style.marginRight = '0.5em';
 				img.style.cursor = 'pointer';
 				img.onclick = TagTooga.Mp3.makeToggle(img, o.href);
 				o.parentNode.insertBefore(img, o);
 				}
 			}
-		}, 
+		},
 
-	toggle: function(img, url) 
-		{ 
-		if (TagTooga.Mp3.playimg == img) 
+	toggle: function(img, url)
+		{
+		if (TagTooga.Mp3.playimg == img)
 			TagTooga.Mp3.destroy();
-		else 
-		{ 
-		if (TagTooga.Mp3.playimg) 
+		else
+		{
+		if (TagTooga.Mp3.playimg)
 			TagTooga.Mp3.destroy();
 		var a = img.nextSibling;
 
-		img.src = mp3StopButton; 
-		TagTooga.Mp3.playimg = img; 
+		img.src = mp3StopButton;
+		TagTooga.Mp3.playimg = img;
 		TagTooga.Mp3.player = document.createElement('span');
 		TagTooga.Mp3.player.innerHTML = 'playing...<object style="vertical-align:bottom;margin-right:0.2em" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"' +
 'codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0"' +
@@ -51,56 +51,44 @@ TagTooga.Mp3 =
 			'<param name="wmode" value="transparent" />' +
 			'<param name="allowScriptAccess" value="sameDomain" />' +
 			'<param name="flashVars" value="theLink='+url+'" />' +
-			'<param name="movie" value="http://www.estoescasa.com/swf/playMp3.swf" /><param name="quality" value="high" />' +
-			'<embed style="vertical-align:bottom;margin-right:0.2em" src="http://www.estoescasa.com/swf/playMp3.swf" flashVars="theLink='+url+'"'+
+			'<param name="movie" value="../swf/playMp3.swf" /><param name="quality" value="high" />' +
+			'<embed style="vertical-align:bottom;margin-right:0.2em" src="../swf/playMp3.swf" flashVars="theLink='+url+'"'+
 			'quality="high" wmode="transparent" width="1" height="1" name="player"' +
 			'align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash"' +
 			' pluginspage="http://www.macromedia.com/go/getflashplayer" /></object>';
 		img.parentNode.insertBefore(TagTooga.Mp3.player, img.nextSibling);
 		}
-		}, 
+		},
 
-	destroy: function() 
-	{ 
-		TagTooga.Mp3.playimg.src = mp3PlayButton; 
-		TagTooga.Mp3.playimg = null; 
-		TagTooga.Mp3.player.removeChild(TagTooga.Mp3.player.firstChild); 
-		TagTooga.Mp3.player.removeChild(TagTooga.Mp3.player.firstChild); 
-		TagTooga.Mp3.player.parentNode.removeChild(TagTooga.Mp3.player); 
+	destroy: function()
+	{
+		TagTooga.Mp3.playimg.src = mp3PlayButton;
+		TagTooga.Mp3.playimg = null;
+		TagTooga.Mp3.player.removeChild(TagTooga.Mp3.player.firstChild);
+		TagTooga.Mp3.player.removeChild(TagTooga.Mp3.player.firstChild);
+		TagTooga.Mp3.player.parentNode.removeChild(TagTooga.Mp3.player);
 		TagTooga.Mp3.player = null;
-	}, 
+	},
 
-	makeToggle: function(img, url) 
-	{ 
+	makeToggle: function(img, url)
+	{
 		return function()
-		{ 
+		{
 			TagTooga.Mp3.toggle(img, url);
 		}
-	} 
-} 
+	}
+}
 
-TagTooga.addLoadEvent = function(f) 
-{ 
+TagTooga.addLoadEvent = function(f)
+{
 	var old = window.onload;
-	
-	if (typeof old != 'function') 
+
+	if (typeof old != 'function')
 		window.onload = f;
-	else 
-		{ 
+	else
+		{
 		window.onload = function() { old(); f() }
-		} 
-} 
+		}
+}
 
 TagTooga.addLoadEvent(TagTooga.Mp3.go);
-
-
-
-
-
-
-
-
-
-
-
-
