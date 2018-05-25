60129949e0fb399386eb5b0a50f8a4aa6244ac39
Unknown <i.jimenezpi@gmail.com>
2018-05-25 15:30:08 +0200

    Cambios cabeceras SEO

--- head.php
+++ head.php
@@ -4,6 +4,13 @@ $root = "/";
 <title>Records del mundo</title>
 <link rel="shortcut icon" href="<?php echo htmlspecialchars($root) ?>images/favicons/globe_bw.png" type="image/x-icon">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="Récords del Mundo es la casa de discos de los componentes de Atención Tsunami junto a Rodrigo (Ediciones Ochoacostado), Pablo (producción) y Emilio y Gonzalo (diseño). Nueve plusmarquistas del amor al arte que aúnan su experiencia previa y paralela en otras plataformas autogestionadas para seguir haciendo, juntos, lo que les gusta."/>
+<meta name="referrer" content="no-referrer-when-downgrade" />
+<meta property="og:title" content="Records del mundo" />
+<meta property="og:type" content="Record label" />
+<meta property="og:url" content="https://recordsdelmundo.es" />
+<meta property="og:image" content="https://www.recordsdelmundo.es/images/fosbury.png" />
+<meta property="og:description" content="Récords del Mundo es la casa de discos de los componentes de Atención Tsunami junto a Rodrigo (Ediciones Ochoacostado), Pablo (producción) y Emilio y Gonzalo (diseño). Nueve plusmarquistas del amor al arte que aúnan su experiencia previa y paralela en otras plataformas autogestionadas para seguir haciendo, juntos, lo que les gusta."/>
 <link href="<?php echo htmlspecialchars($root) ?>style/fonts.css" type="text/css" rel="stylesheet" />
 <link href="<?php echo htmlspecialchars($root) ?>style/common.css" type="text/css" rel="stylesheet" />
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" defer=true></script>
