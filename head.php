9d6a880eda15919e375d570d6377fff9da333e9d
Unknown <i.jimenezpi@gmail.com>
2019-03-22 01:26:48 +0100

    Added integrity checking to js libraries

--- head.php
+++ head.php
@@ -13,5 +13,9 @@ $root = "/";
 <meta property="og:description" content="Récords del Mundo es la casa de discos de los componentes de Atención Tsunami junto a Rodrigo (Ediciones Ochoacostado), Pablo (producción) y Emilio y Gonzalo (diseño). Nueve plusmarquistas del amor al arte que aúnan su experiencia previa y paralela en otras plataformas autogestionadas para seguir haciendo, juntos, lo que les gusta."/>
 <link href="<?php echo htmlspecialchars($root) ?>style/fonts.css" type="text/css" rel="stylesheet" />
 <link href="<?php echo htmlspecialchars($root) ?>style/common.css" type="text/css" rel="stylesheet" />
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" defer=true></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"
+    defer=true
+    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossorigin="anonymous">
+</script>
 <script src="<?php echo htmlspecialchars($root) ?>scripts/main.js" defer=true></script>
