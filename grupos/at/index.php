3ad3da109adf83adf3b41ef7e76bfba10457a906
Unknown <i.jimenezpi@gmail.com>
2019-03-28 23:06:32 +0100

    Cambiada pagina de AT con VLTRA y nota de prensa

--- grupos/at/index.php
+++ grupos/at/index.php
@@ -32,7 +32,7 @@ require("../../head.php");
 				<br>
 				<table class="tablagrupo">
 					<tr>
-						<td class="datos"><a href="AT_SELR_Prensa.pdf"><span>Nota de prensa</span></a> · <a href="rider_at.pdf"><span>Rider</span></a></td>
+						<td class="datos"><a href="AT_VLTRA_Prensa.pdf"><span>Nota de prensa</span></a> · <a href="rider_at.pdf"><span>Rider</span></a></td>
 						<td class="social">
 						<a class="iconos facebook" href="http://www.facebook.com/atencion.tsunami"></a>
 						<a class="iconos instagram" href="http://instagram.com/atenciontsunami"></a>
@@ -40,32 +40,30 @@ require("../../head.php");
 						</td>
 					</tr>
 				</table>
+
+
 				<p>
-<strong>Atención tsunami</strong> vuelven a superarse y firman con <i>Silencio en la retaguardia</i> su mejor disco (el tercero) hasta la fecha, una formidable colección de canciones que llevan aún más lejos los presupuestos del brillante <i>Que le corten la cabeza (RDM 2014)</i>.
+					Tres años después de <i>Silencio en la retaguardia (RDM 2016)</i> —mejor disco español del año para El
+					Cultural de El Mundo y mejor disco del año en Madrid para la revista Mondosonoro— <strong>Atención Tsunami</strong>
+					regresan al ruido ibérico con <i>VLTRA</i>.
 				</p>
 				<p>
-El exilio ultramarino de Álvaro durante dos años les privó de girar su anterior
-referencia como merecía, pero produjo una acumulación de energía,
-ideas y ganas de revancha que han cristalizado en 9 canciones esbozadas
-en apenas tres meses y completadas y grabadas durante la Semana Santa
-de 2016 con <b>Raúl Pérez</b>, de nuevo en <b>La Mina</b> (Sevilla), y mezcladas
-y masterizadas con mimo por <b>Manuel Cabezalí</b> (quien se ha encargado
-también de algún recording posterior). El grupo no dudó un segundo en
-repetir con los mismos guardaespaldas técnicos y el resultado no podía
-haber sido mejor.
+					Tras una larga gestación, la criatura ha salido fiera y hermosa: ocho cortes crudos y directos, rebosantes
+					de gasolina, lírica y mucho whammy (¿quién quiere autotune teniendo <i>whammy</i>?). Esta vez, el ritmo y la
+					palabra ganan la partida a la melodía, y la urgencia a la introspección. <i>Vltra</i> («más allá») es un disco ambivalente,
+					un viaje que aborda la sensación planetaria de ir de cabeza hacia al abismo, pero que se aferra en el último momento a la esperanza
+					de que los círculos viciosos (los propios mentales y los de regresión política y social) pueden y deben
+					romperse. Claro que sí.
 				</p>
 				<p>
-El resultado es un trabajo poliédrico e intenso, complejo y disfrutable a
-partes iguales, y en el que lo musical y lo narrativo (los textos) se imbrican
-con fuerza. Algo que responde también al interés del grupo por artistas y
-producciones de diferentes épocas que se abstienen de la fidelidad a un
-concepto o fórmula y de su explotación reiterada para aventurarse y explorar
-territorios y sonidos diferentes en cada canción.
+					<strong>Disoriented Adult Rock para tiempos oscuros.</strong>
 				</p>
 				<br>
+				<!--
 				<script>
 				setTimeout(function(){ $("#contenido").append('<iframe src="https://www.youtube.com/embed/85ythvl7wU4?rel=0"  min-width="660px" width="100%" height="405px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe><br><br>'); }, 200);
 				</script>
+			-->
 			</div>
 		</div>
 	</div>
